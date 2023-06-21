bd554cac00d6d8ee52f565b1dc530534e0154547
bartbory <bartosz.borycki@gmail.com>
2023-06-21 15:05:55 +0200

    topBar style fix

--- components/backoffice/TopBar.vue
+++ components/backoffice/TopBar.vue
@@ -29,7 +29,7 @@ header {
   align-items: center;
   padding: 0 8px;
   color: var(--white-900);
-  width: 100vw;
+  width: 100%;
   height: 48px;
   background: var(--gradient-purple);
 }
@@ -42,37 +42,13 @@ header {
   font-family: "Krona One";
   font-size: 12px;
 }
-.avatar {
-  width: 24px;
-  height: 24px;
-  border: 1px solid var(--white-900);
-  border-radius: 50%;
-}
 @media screen and (min-width: 740px) {
   header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
     padding: 0 24px;
-    color: var(--white-900);
-    width: 100vw;
-    height: 64px;
-    background: var(--gradient-purple);
   }
 
   .user {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
     column-gap: 16px;
-    font-family: "Krona One";
-    font-size: 12px;
-  }
-  .avatar {
-    width: 48px;
-    height: 48px;
-    border: 1px solid var(--white-900);
-    border-radius: 50%;
   }
 }
 </style>
