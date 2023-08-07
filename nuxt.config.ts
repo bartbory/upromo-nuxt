b8d9850529a6a72dd9c87853358cafd527abac01
bartbory <bartosz.borycki@gmail.com>
2023-08-07 12:28:52 +0200

    gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/supabase"],
-
+  modules: ["@nuxtjs/supabase", "@nuxtjs/google-analytics"],
+  plugins: ["@/plugins/vue-gtag"],
   css: ["@fortawesome/fontawesome-svg-core/styles.css"],
 });
