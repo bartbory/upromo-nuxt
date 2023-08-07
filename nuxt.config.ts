2631a50f5b91df762b11312050cfc8d8ea25d076
bartbory <bartosz.borycki@gmail.com>
2023-08-07 12:52:29 +0200

    gtag fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/supabase"],
-  plugins: ["@/plugins/gtag"],
+  modules: ["@nuxtjs/supabase", 'nuxt-gtag'],
+  gtag: {
+    id: "G-9X809BG7PK"
+  },
   css: ["@fortawesome/fontawesome-svg-core/styles.css"],
 });
--- plugins/gtag.js
+++
@@ -1,5 +0,0 @@
-import VueGtag from "vue-gtag";
-
-export default Vue.use(VueGtag, {
-  config: { id: "G-9X809BG7PK" },
-});
